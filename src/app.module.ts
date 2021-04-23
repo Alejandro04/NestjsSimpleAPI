318744c55790871577758c57984ca1c4497aca66
Alejandro Roa <alejandro.roa@globalesm.com>
2021-04-23 16:04:49 -0400

    init

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: false
+    }),
+    TasksModule
+  ],
+})
+export class AppModule {}
---
+++ src/tasks/services/tasks.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from '../entities/tasks.entity';
+
+@Injectable()
+export class TasksService {
+
+  constructor(@InjectRepository(Task) private taskRepo: Repository<Task>
+  ) { }
+
+  findAll() {
+    return this.taskRepo.find();
+  }
+
+  findOne(id: number) {
+    return this.taskRepo.findOne(id);
+  }
+
+  create(body: any) {
+    const newTask = this.taskRepo.create({
+      name: body.name,
+      description: body.description
+    });
+    return this.taskRepo.save(newTask);
+  }
+
+  async update(id: number, body: any) {
+    const task = await this.taskRepo.findOne(id);
+    this.taskRepo.merge(task, body);
+    return this.taskRepo.save(task);
+  }
+
+  async delete(id: number) {
+    await this.taskRepo.delete(id);
+    return true;
+  }
+}
