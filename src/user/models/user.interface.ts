40c6dd812bd38c73a789308f8a6700cdde22e0bf
Alejandro Roa <alejandro.roa@pelikane.com>
2021-05-03 09:53:50 -0400

    config user model and auth service

---
+++ src/user/models/user.interface.ts
@@ -0,0 +1,14 @@
+export interface User {
+    id?: number;
+    name?: string;
+    username?: string;
+    email?: string;
+    password?: string;
+    role?: UserRole;
+    profileImage?: string;
+}
+
+export enum UserRole {
+    ADMIN = 'admin',
+    USER = 'user'
+}
