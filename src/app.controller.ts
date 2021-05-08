8b47328cd5277890a645be2e2cff08145af316de
Alejandro Roa <alejandro.roa@pelikane.com>
2021-05-08 12:52:50 -0400

    clean code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,5 @@
 import { Controller, Get, Post, Request, SetMetadata, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth/services/auth.service';
-import { Roles } from './auth/decorators/roles.decorator';
-import { Role } from './auth/enums/role.enums';
-import { AuthGuard } from '@nestjs/passport';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Controller()
